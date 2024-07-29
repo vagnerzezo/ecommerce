445e12ba87d3f0b410623031d99071450395bde2
vagner zezo <57641762+vagnerzezo@users.noreply.github.com>
2024-07-29 11:53:34 -0300

    fixed performance

--- src/components/home/banner-meio.vue
+++ src/components/home/banner-meio.vue
@@ -4,14 +4,13 @@
       <img v-if="isMobile" srcset="
         @/assets/img/banner/banner-Meio-mob.webp 600w
       " sizes="(max-width: 300px) 300px, (max-width: 450px) 450px, 600px"
-         src="@/assets/img/banner/banner-Meio-mob.webp" alt="bannerMeio" loading="lazy" />
+         src="@/assets/img/banner/banner-Meio-mob.webp" alt="bannerMeio" loading="lazy" width="600" height="auto" />
       <img v-else srcset="
         @/assets/img/banner/banner-Meio.webp 6912w
       " sizes="(max-width: 2304px) 2304px, (max-width: 4608px) 4608px, 6912px"
-         src="@/assets/img/banner/banner-Meio.webp" alt="bannerMeio" loading="lazy" />
+         src="@/assets/img/banner/banner-Meio.webp" alt="bannerMeio" loading="lazy" width="6912" height="auto" />
    </div>
 </template>
-
 <script>
 export default {
    name: 'BannerMeio',
--- src/components/home/fullbanner.vue
+++ src/components/home/fullbanner.vue
@@ -3,15 +3,15 @@
    <div class="fullbanner">
       <div v-if="isMobile">
          <img srcset="
-          @/assets/img/banner/fullbanner-mob.webp 600w,
+          @/assets/img/banner/fullbanner-mob.webp 600w
         " sizes="(max-width: 600px) 600px, (max-width: 900px) 900px, 1200px"
-            src="@/assets/img/banner/fullbanner-mob.webp" alt="fullbanner" loading="lazy" />
+            src="@/assets/img/banner/fullbanner-mob.webp" alt="fullbanner" loading="lazy" width="1200" height="auto" />
       </div>
       <div v-else>
          <img srcset="
           @/assets/img/banner/fullbanner.webp 800w
         " sizes="(max-width: 800px) 800px, (max-width: 1200px) 1200px, 1900px"
-            src="@/assets/img/banner/fullbanner.webp" alt="fullbanner" loading="lazy" />
+            src="@/assets/img/banner/fullbanner.webp" alt="fullbanner" loading="lazy" width="1900" height="auto" />
       </div>
    </div>
 </template>
